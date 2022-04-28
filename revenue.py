5d445ae170c2381692aecbcae8b9ccde2c0c0a8b
mindt102 <minhdt24082002@gmail.com>
2022-04-28 13:24:13 +0700

    load random data and show booking id

--- revenue.py
+++ revenue.py
@@ -1,5 +1,7 @@
+import json
+import random
 from unicodedata import name
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.uic import loadUi
 from const import *
 from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QWidget, QCalendarWidget, QLabel
@@ -13,48 +15,69 @@ class Revenue(QtWidgets.QWidget):
         self.setFixedSize(CONTENT_WIDTH, WINDOW_HEIGHT)
         #set length of columns in table
         self.table_Revenue.setColumnWidth(0,225)
-        self.table_Revenue.setColumnWidth(1,160)
-        self.table_Revenue.setColumnWidth(2,160)
-        self.table_Revenue.setColumnWidth(3,160)
-
-        #Set calandar up
-        self.calendar = self.findChild(QCalendarWidget, "calendarWidget")
-        self.label = self.findChild(QLabel, "Calendar_label")
+        self.table_Revenue.setColumnWidth(1,135)
+        self.table_Revenue.setColumnWidth(2,135)
+        self.table_Revenue.setColumnWidth(3,135)
+        self.table_Revenue.setColumnWidth(4,135)
 
         #Connect calendar to the function
-        self.calendar.selectionChanged.connect(self.grab_date)
+        # self.date = self.calendarWidget.selectedDate()
         #load data
-        self.loaddata()
-        self.Button_booking()
+        self.reloadData()
 
-    def grab_date(self):
-        dateSelected = self.calendar.selectedDate()
-        #Put date on label
-        self.label.setText(str(dateSelected.toString()))    
+        self.calendarWidget.selectionChanged.connect(self.reloadData)
+        self.detailsBtn.clicked.connect(self.showBookingDetails)
+
     
-    #Button
-    def Button_booking(self):
-        self.Btn_booking.clicked.connect(self.Booking_Details)
-    
-    def loaddata(self):
-        data = [{'booking id':'777' ,'name':"Cristiano Ronaldo",'room_num':201,'room_fee': 200, 'services_fee':200},
-        {'booking id': '10','name': 'Lionel Messi', 'room_num':202,'room_fee':200,'services_fee':200},
-        {'booking id': '9','name': 'Zlatan Imbrahimovic', 'room_num': 203, 'room_fee':400, 'services_fee': 450}]
+    def reloadData(self):
+        _translate = QtCore.QCoreApplication.translate
+
+        self.date = self.calendarWidget.selectedDate()
+        self.calendarLabel.setText(_translate("Revenue", self.date.toString()))
+        self.loadData()
+
+    def loadData(self):
+        # data = [{'booking id':'777' ,'name':"Cristiano Ronaldo",'room_num':201,'room_fee': 200, 'services_fee':200},
+        # {'booking id': '10','name': 'Lionel Messi', 'room_num':202,'room_fee':200,'services_fee':200},
+        # {'booking id': '9','name': 'Zlatan Imbrahimovic', 'room_num': 203, 'room_fee':400, 'services_fee': 450}]
+        self.data = self.getRevenueByDate(self.date.toString("yyyy-MM-dd"))
         row = 0
-        self.table_Revenue.setRowCount(len(data))
-        for i in range(len(data)):
-            #self.table_Revenue.setItem(row, -1, QtWidgets.QTableWidgetItem(str(data[i]["booking id"])))
-            self.table_Revenue.setItem(row, 0, QtWidgets.QTableWidgetItem(data[i]["name"]))
-            self.table_Revenue.setItem(row, 1, QtWidgets.QTableWidgetItem(str(data[i]['room_num'])))
-            self.table_Revenue.setItem(row, 2, QtWidgets.QTableWidgetItem(str(data[i]['room_fee'])))
-            self.table_Revenue.setItem(row, 3, QtWidgets.QTableWidgetItem(str(data[i]['services_fee'])))
+        self.table_Revenue.setRowCount(len(self.data))
+        for i in range(len(self.data)):
+            #self.table_Revenue.setItem(row, -1, QtWidgets.QTableWidgetItem(str(self.data[i]["booking id"])))
+            self.table_Revenue.setItem(row, 0, QtWidgets.QTableWidgetItem(self.data[i]["clientName"]))
+            self.table_Revenue.setItem(row, 1, QtWidgets.QTableWidgetItem(str(self.data[i]['roomNumber'])))
+            self.table_Revenue.setItem(row, 2, QtWidgets.QTableWidgetItem(str(self.data[i]['roomFee'])))
+            self.table_Revenue.setItem(row, 3, QtWidgets.QTableWidgetItem(str(self.data[i]['serviceFee'])))
+            self.table_Revenue.setItem(row, 4, QtWidgets.QTableWidgetItem(str(self.data[i]['totalBill'])))
             row = row + 1
     
+    def getRevenueByDate(self, date: str) -> list:
+        bookingsByDate = []
+        with open(DATAPATH + "bookings.json", "r") as f:
+            bookings = json.load(f)
+            for booking in bookings:
+                if booking["checkoutDate"] == date and booking["status"] == 3:
+                    bookingsByDate.append(booking)
+        revenuesByDate = []
+        for booking in bookingsByDate:
+            revenue = {
+                "bookingId": booking["id"],
+                "clientName": booking["clientName"],
+                "roomNumber": booking["roomNumber"],
+                "roomFee": random.randint(300, 500),
+                "serviceFee": random.randint(100, 300),
+            }
+            revenue["totalBill"] = revenue["roomFee"] + revenue["serviceFee"]
+            revenuesByDate.append(revenue)
+        return revenuesByDate
+    
+    def getSelectedBookingId(self):
+        return self.data[self.table_Revenue.currentRow()]["bookingId"]
+
     #Event of button
-    def Booking_Details(self):
-        row = self.table_Revenue.currentRow()
-        current_name = (self.table_Revenue.item(row,0).text())
-        print(current_name)
+    def showBookingDetails(self):
+        print(self.getSelectedBookingId())
 
 if __name__ == "__main__":
     import sys
